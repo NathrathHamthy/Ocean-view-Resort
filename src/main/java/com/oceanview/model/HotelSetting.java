6954ab3f202f1e7c0231f865425e199703d33b94
NathrathHamthy <nathrathhamthy@gmail.com>
2026-02-28 11:28:45 +0530

    Add HotelSetting constructors and accessors

--- src/main/java/com/oceanview/model/HotelSetting.java
+++ src/main/java/com/oceanview/model/HotelSetting.java
@@ -23,4 +23,90 @@ public class HotelSetting implements Serializable {
     private boolean editable;
     private LocalDateTime updatedAt;
     private Integer updatedBy;
+
+    public HotelSetting() {
+        this.settingType = SettingType.STRING;
+        this.editable = true;
+    }
+
+    public HotelSetting(String category, String key, String value, SettingType type, String description) {
+        this.category = category;
+        this.settingKey = key;
+        this.settingValue = value;
+        this.settingType = type;
+        this.description = description;
+        this.editable = true;
+    }
+
+    public int getSettingId() {
+        return settingId;
+    }
+
+    public void setSettingId(int settingId) {
+        this.settingId = settingId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getSettingKey() {
+        return settingKey;
+    }
+
+    public void setSettingKey(String settingKey) {
+        this.settingKey = settingKey;
+    }
+
+    public String getSettingValue() {
+        return settingValue;
+    }
+
+    public void setSettingValue(String settingValue) {
+        this.settingValue = settingValue;
+    }
+
+    public SettingType getSettingType() {
+        return settingType;
+    }
+
+    public void setSettingType(SettingType settingType) {
+        this.settingType = settingType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isEditable() {
+        return editable;
+    }
+
+    public void setEditable(boolean editable) {
+        this.editable = editable;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Integer updatedBy) {
+        this.updatedBy = updatedBy;
+    }
 }
