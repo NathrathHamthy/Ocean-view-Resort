5077e50c1a5db99886a3eb958aa38a736f7df112
NathrathHamthy <nathrathhamthy@gmail.com>
2026-02-28 11:28:26 +0530

    Add HotelSetting model skeleton

---
+++ src/main/java/com/oceanview/model/HotelSetting.java
@@ -0,0 +1,26 @@
+﻿package com.oceanview.model;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * Hotel setting entity.
+ */
+public class HotelSetting implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    public enum SettingType {
+        STRING, INTEGER, DECIMAL, BOOLEAN, JSON
+    }
+
+    private int settingId;
+    private String category;
+    private String settingKey;
+    private String settingValue;
+    private SettingType settingType;
+    private String description;
+    private boolean editable;
+    private LocalDateTime updatedAt;
+    private Integer updatedBy;
+}
